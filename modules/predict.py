b710c2c551f51e144b374d4d02a2eaf0597323f8
Ralina <Ralinalmetova@gmail.com>
2024-02-06 10:14:03 +0400

    Homework_modul_33

---
+++ modules/predict.py
@@ -0,0 +1,31 @@
+import json
+import joblib
+import pandas as pd
+import dill
+import os
+from datetime import datetime
+from sklearn.pipeline import Pipeline
+
+
+path = os.environ.get('PROJECT_PATH', '..')
+def predict():
+    models = os.listdir(f'{path}/data/models')
+    with open(f'{path}/data/models/{models[-1]}', 'rb') as file:
+        model = dill.load(file)
+    df_pred = pd.DataFrame(columns=['car_id','pred'])
+    files_list = os.listdir(f'{path}/data/test')
+    for filename in files_list:
+        with open(f'{path}/data/test/{filename}') as file:
+            form = json.load(file)
+        data = pd.DataFrame.from_dict([form])
+        prediction = model.predict(data)
+        dict_pred = {'car_id': data.id, 'pred': prediction}
+        df = pd.DataFrame(dict_pred)
+        df_pred = pd.concat([df_pred,df], axis=0)
+
+    df_pred.to_csv(f'{path}/data/predictions/preds_{datetime.now().strftime("%Y%m%d%H%M")}.csv', index=False)
+    pass
+
+
+if __name__ == '__main__':
+    predict()
